6e217f3b0964edee80e3961d5f911328d6ea7406
shehabadel <shehabeldinadel@gmail.com>
2025-02-22 10:59:27 +0200

    Injected myService inside the controller

--- Lab1/src/main/java/com/example/demo/controllers/MyController.java
+++ Lab1/src/main/java/com/example/demo/controllers/MyController.java
@@ -1,11 +1,21 @@
 package com.example.demo.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.example.demo.services.MyService;
 
 @RestController
 public class MyController {
+    private final MyService myService;
+
+    @Autowired
+    public MyController(MyService myService) {
+        this.myService = myService;
+    }
+
     @GetMapping("/hello")
     public String sayHello() {
+        myService.doSomething();
         return "Hello, World!";
     }
 }
