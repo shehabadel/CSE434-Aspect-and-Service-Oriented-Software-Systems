36785621c0a13a3cf1df86f4dd7b5f129fdd2281
Shehab Adel <shehabeldinadel@gmail.com>
2025-03-30 17:07:48 +0200

    init service-reservation

---
+++ Lab5/service-reservation/src/main/java/com/example/service_reservation/ServiceReservationApplication.java
@@ -0,0 +1,13 @@
+package com.example.service_reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceReservationApplication.class, args);
+	}
+
+}
---
+++ Lab5/service-reservation/src/test/java/com/example/service_reservation/ServiceReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.service_reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
