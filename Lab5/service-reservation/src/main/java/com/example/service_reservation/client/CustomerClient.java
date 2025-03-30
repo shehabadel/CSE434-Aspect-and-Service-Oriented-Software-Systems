c4d8c44c3c1eb8c611d550ac857255b0d4e083a5
Shehab Adel <shehabeldinadel@gmail.com>
2025-03-30 21:15:12 +0200

    updated customer client

--- Lab5/service-reservation/src/main/java/com/example/service_reservation/client/CustomerClient.java
+++ Lab5/service-reservation/src/main/java/com/example/service_reservation/client/CustomerClient.java
@@ -2,6 +2,7 @@ package com.example.service_reservation.client;
 
 import com.example.service_reservation.dto.CustomerDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -11,14 +12,17 @@ import org.springframework.web.util.UriComponentsBuilder;
 public class CustomerClient {
 
     private final RestTemplate restTemplate;
+    private final String customerServiceUrl;
 
     @Autowired
-    public CustomerClient(RestTemplateBuilder builder) {
+    public CustomerClient(RestTemplateBuilder builder, 
+                        @Value("${service.customer.url}") String customerServiceUrl) {
         this.restTemplate = builder.build();
+        this.customerServiceUrl = customerServiceUrl;
     }
 
     public CustomerDTO getCustomerById(Long customerId) {
-        String url = UriComponentsBuilder.fromHttpUrl("http://service-customer:5000/customers/{id}")
+        String url = UriComponentsBuilder.fromHttpUrl(customerServiceUrl + "/customers/{id}")
                                          .buildAndExpand(customerId)
                                          .toUriString();
         return restTemplate.getForObject(url, CustomerDTO.class);
